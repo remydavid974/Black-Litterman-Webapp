cfa1944aa654ff5cc405b10fc5dcbb08e22b125d
remydavid974 <remy.dav@hotmail.fr>
2025-06-02 23:47:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 import altair as alt
+import plotly.graph_objects as go
 
 st.set_page_config(page_title="Black-Litterman Strategy", layout="wide")
 
